f9648623ee3339a9bd4526a2bf7157d2312de33d
Syed Huzaifa Abid <62389218+huzaifa26@users.noreply.github.com>
2022-11-13 21:30:26 +0500

    add-EventDescription

--- src/components/EventDescriptions/ContactBar.tsx
+++ src/components/EventDescriptions/ContactBar.tsx
@@ -1,7 +1,12 @@
 import React from 'react'
 
+type Props={
+  id:string,
+  heading:string,
+  text:string
+}
 
-export default function ContactBar({heading,text}: any) {
+export default function ContactBar({heading,text}: Props) {
   return (
     <div className='flex justify-between items-center px-[23px] my-[4px] w-[71.296%] h-[74px] bg-[#fed4c3] m-auto'>
       <h3 className='font-[700] text-[20px] leading-[33px] text-[#231414]'>{heading}</h3>
